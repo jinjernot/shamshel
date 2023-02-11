a06bb4a06b7cfb9db70f4ab631a4c76de53a2b16
norindes <norindes@gmail.com>
2023-02-10 23:01:21 -0600

    pm v1.0

---
+++ main.py
@@ -0,0 +1,50 @@
+import xml.etree.ElementTree as ET
+from lxml import etree
+import requests
+
+def fromFile():
+
+    parser = etree.XMLParser(recover=True)
+    tree = ET.parse('./XML/test.xml', parser)
+
+    root = tree.getroot()
+    tags = [elem.tag for elem in root.iter()]
+    mandatory_tags = ["verga","osinstalled", "ioports", "processor", "chipset","image"]
+
+    missing_elements = []
+    for i in mandatory_tags:
+        if i not in tags:
+            missing_elements.append(i)
+
+    print(missing_elements)
+
+def fromURL():
+
+    url= "https://cap.corp.hp.com/capui/productFileOpen.action?path=/var/opt/cap-out/xmlfiles/fullload/ww-en/021015xxxxxx/021015649xxx/2101564959.xml"
+    response = requests.get(url, verify=False)
+    xml_content = response.content
+    root = ET.fromstring(xml_content)
+    tags = [elem.tag for elem in root.iter()]
+    mandatory_tags = ["osinstalled", "ioports", "processor", "chipset","image"]
+
+    missing_elements = []
+    for i in mandatory_tags:
+        if i not in tags:
+            missing_elements.append(i)
+
+    print(missing_elements)
+
+def main():
+    print("Bienvenido a Predator-Mutator")
+    user_input = input("1 para file, 2 para URL ")
+    x = int(user_input)
+
+    if x == 1:
+        print("elegiste 1, leyendo el xml local")
+        fromFile()
+    elif x == 2:
+        print("elegiste 2, leyendo el xml desde URL")
+        fromURL()
+
+if __name__ == "__main__":
+    main()
