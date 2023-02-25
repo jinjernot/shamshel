e10ec9af28074aa62313098ce5b8c1f121368c9d
norindes <norindes@gmail.com>
2023-02-25 15:43:50 -0600

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # predator-mutator
-Trying to automate my task to review XML files
+
+I'm tired of looking at XMLs, so i'll automate this task.
+
+Review the mandatory tag, if not found it is returned in a dataframe, capable of reading multiple files
+
+Version 1.0
+Laptops and Desktops techspecs
--- main.py
+++ main.py
@@ -4,18 +4,26 @@ import glob
 import pandas as pd
 import os
 
-
 def fromFile():
     """Read the XMLs from the folder"""
     parser = etree.XMLParser(recover=True)
     folder_path = "./XML/"
-    csv_path = "./csv/"
+    csv_path = "./csv"
     xml_files = glob.glob(folder_path + "*.xml")
 
     for xml_file in xml_files: #loop through all the files
         tree = ET.parse(xml_file, parser)
         root = tree.getroot()
         searchTags(root, csv_path, xml_file)
+    
+    dfs = []
+    for filename in os.listdir(csv_path): #loop through all the csvs
+        if filename.endswith('.csv'):
+            filepath = os.path.join(csv_path, filename)
+            df = pd.read_csv(filepath)
+            dfs.append(df)
+    concat_df = pd.concat(dfs, axis=1)
+    concat_df.to_csv('missing_content.csv', index=False)
 
 def searchTags(root, csv_path, xml_file):
     """Search for mandatory tags"""
@@ -26,43 +34,55 @@ def searchTags(root, csv_path, xml_file):
     if product_type["pmoid"] == "12454": # Desktops & Workstations
         print("This SKU is from Hierarchy: " + product_type["name"])
         print(sku_number + ":")
-        mandatory_tags = ["osinstalled", "ioports", "processor", "chipset","image"]
+        mandatory_tags = ["chassistype","marketing_tier","prodshortnamespecs","prodlongnamespecs","osinstalleddes01","osinstalled","chipset","processorfamily","processortype","processorname","memmax_01","memslots","memstdes_01","memstnote_01","memstosimp","hdcntrltype_01","hdcntrltype_01hdmin","hdcntrltype_01hdmax","hd_01des","filter_storagetype","expanslots","ioports_01_location","ioports","netinterface","managefeatures","swincluded","securitymgmt","weightmet","weightus","weightpackmet","weightpackus","dimenmet","dimenus","dimenpackmet","dimenpackus","powersupply","powersupplyfixed","upc","wrntyfeatures"]
         missing_elements = []
         for i in mandatory_tags:
             if i not in tags:
                 missing_elements.append(i)
         DesktopDF = pd.DataFrame({"XML Missing": missing_elements})
-        print(DesktopDF.to_string(index=False))
-        print(xml_file)
+        new_row = {'XML Tags Missing' : sku_number}
+        DesktopDF.loc[-1] = new_row
+        DesktopDF.index = DesktopDF.index + 1
+        DesktopDF = DesktopDF.sort_index()
+
         csv_filename = os.path.splitext(xml_file)[0] + ".csv"
-        xml_file = xml_file.replace("./XML", "")
-        print(xml_file)
+        csv_filename = csv_filename.replace("./XML/","")
         DesktopDF.to_csv(os.path.join(csv_path, csv_filename), index=False)
 
     elif product_type["pmoid"] == "18972": # Printers
         print("This SKU is from Hierarchy: " + product_type["name"])
         print(sku_number + ":")
-        mandatory_tags = ["osinstalled", "ioports", "processor", "chipset","image"]
+        mandatory_tags = [""]
         missing_elements = []
         for i in mandatory_tags:
             if i not in tags:
                 missing_elements.append(i)
         PrintDF = pd.DataFrame({"XML Missing": missing_elements})
-        print(PrintDF.to_string(index=False))
-        PrintDF.to_csv("./csv/print.csv", index=False)
-        
+        new_row = {'XML Tags Missing' : sku_number}
+        PrintDF.loc[-1] = new_row
+        PrintDF.index = PrintDF.index + 1
+        PrintDF = PrintDF.sort_index()
+
+        csv_filename = os.path.splitext(xml_file)[0] + ".csv"
+        csv_filename = csv_filename.replace("./XML/","")
+        PrintDF.to_csv(os.path.join(csv_path, csv_filename), index=False)
 
     elif product_type["pmoid"] == "321957": # Laptops
         print("This SKU is from Hierarchy: " + product_type["name"])
         print(sku_number + ":")
-        mandatory_tags = ["batterylife","batteryrechrg","batterytype","batteryweightimp","batteryweightmet","cdromdvd","cloudserv","dispdescmetshort","dispdescshort","display","displaybright","displaycolorgamut","displaymet","displaysize","displaysizemet","ecohighlighgraphic","energyeffcomp","filter_processorspd","filter_storagetype","fingerprread","flashcache","flashcachenote","gcformfactor","gcnote","graphicseg_01card_01","graphicseg_01header","graphicseg_02card_01","graphicseg_02header","hd_01des","hd_02des","keybrd","maxbatterylifevideo","memlayout","memstdes_01","memstdnote","memstosimp","mousepntgdevice","osinstalled","osinstallednote","powersupplytype","processorcache","processorcoreinstalled","processorfamily","processorname","prodfinish","productcolour","storage_acceleration","swincluded","swpreinstalled","swprodfinance","webcam","wirelesstech"]
+        mandatory_tags = ["marketing_tier","graphicseg_01header","graphicseg_01card_01","productcolour","prodfinish","weightmet","weightmetnote","weightus","weightpackmet","weightpackus","dimenmet","dimenus","dimenpackmet","dimenpackus","batterytype","batterylife","batteryrechrg","batteryrechrgftntnbr","batterytypenote","powersupplytype","webcam","ioports","wirelesstech","wirelesstechnonote","displaysize","displaysizemet","display","displaymet","screenbodyratio","displaycolorgamut","displaybright","filter_storagetype","cloudserv","cloudservftntnbr","hd_01des","memstdes_01","memstdnote","keybrd","mousepntgdevice","audiofeat","chipset","processorname","processornameftntnbr","processorfamily","swincluded","swprodfinance","osinstalled"]
         missing_elements = []
         for i in mandatory_tags:
             if i not in tags:
                 missing_elements.append(i)
         LaptopDF = pd.DataFrame({"XML Tags Missing": missing_elements})
-        print(LaptopDF.to_string(index=False))
-        LaptopDF.to_csv("./csv/laptop.csv", index=False)
+        new_row = {'XML Tags Missing' : sku_number}
+        LaptopDF.loc[-1] = new_row
+        LaptopDF.index = LaptopDF.index + 1
+        LaptopDF = LaptopDF.sort_index()
+        csv_filename = os.path.splitext(xml_file)[0] + ".csv"
+        csv_filename = csv_filename.replace("./XML/","")
+        LaptopDF.to_csv(os.path.join(csv_path, csv_filename), index=False)
 
 def main():
     print("Bienvenido a Predator-Mutator")
